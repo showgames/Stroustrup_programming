f07bfaca9328dbce6c1243275d3d9817f6d34cbc
Shohei Hayashi <2329shohei@gmail.com>
2015-11-05 22:09:32 +0900

    add 5_1.cpp

---
+++ 5章/5_1.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  Practice
+//
+//  Created by forest_hornet on 2015/10/10.
+//  Copyright © 2015年 Shohei Hayashi. All rights reserved.
+
+// 摂氏(セルシウス度)を絶対温度に変換
+
+#include "../std_lib_facilities.h"
+
+double
+ctok(double c)
+{
+    const double kelvin = 273.15;
+    return c + kelvin;
+}
+
+int
+main()
+{
+    double c = 0;
+    cin >> c;
+    double k = ctok(c);
+    cout << k << endl;
+}
